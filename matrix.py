21c1b1102fbd83ce4521a5153e1975007e27583a
william Forsey <billforsey@gmail.com>
2024-09-19 14:25:53 +0100

    Update matrix.py
    I assume this is what yout wanted in the issue log when talking about creating a zeros matrix

--- matrix.py
+++ matrix.py
@@ -3,3 +3,13 @@ from typing import List
 class Matrix:
   def __init__(self,data:List):
     self.data = data
+  
+  # making a matrix of 0's of any size
+
+  cols = int(input('enter the number of columns: '))
+  rows = int(input('enter the number of rows: '))
+  M = []
+  for i in range(rows):
+      M.append([])
+      for j in range(cols):
+          M[i].append(0)
